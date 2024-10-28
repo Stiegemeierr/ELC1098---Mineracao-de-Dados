f1077a46e2d8fb06bf3206f6e41544ffb8f5d23f
Stiegemeier <gabrielstiegemeier@gmail.com>
2024-10-28 16:03:48 -0300

    fix-preprocess-remove-sweet-type
    Corrige erro na fase de pré-processamento e remove o tipo 'doce' do dataset.

--- Trabalho1/scriptremovedoce.py
+++ Trabalho1/scriptremovedoce.py
@@ -1,17 +1,22 @@
 import json
+import re
 
 
 arquivo_json = r'C:\Users\Gabriel\Documents\GitHub\ELC1098---Mineracao-de-Dados\Trabalho1\padaria_trab.json'
 
 
-
 with open(arquivo_json, 'r', encoding='utf-8') as f:
     dados = json.load(f)
 
-for i, item in enumerate(dados, start=1):
-    item["compra"] = i
+
+for item in dados:
+    item["produtos"] = [
+        re.sub(r"^Doce\s+\w+", "Doce", produto) if produto.startswith("Doce") else produto
+        for produto in item["produtos"]
+    ]
+
 
 with open(arquivo_json, 'w', encoding='utf-8') as f:
     json.dump(dados, f, ensure_ascii=False, indent=4)
 
-print("O arquivo JSON foi atualizado com o incremento de 'compra'!")
+print("O arquivo JSON foi atualizado com 'Doce' sem o tipo!")
