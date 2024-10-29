f51f9b3899085272adacc7cb6ce17d73d251152b
Stiegemeier <gabrielstiegemeier@gmail.com>
2024-10-29 13:39:04 -0300

    Add Dataset1, Dataset2 and some fixes

--- Trabalho1/script.py
+++ Trabalho1/script.py
@@ -1,9 +1,6 @@
 import json
 
-
-arquivo_json = r'C:\Users\Gabriel\Documents\GitHub\ELC1098---Mineracao-de-Dados\Trabalho1\padaria_trab.json'
-
-
+arquivo_json = r'C:\Users\gabri\OneDrive\Documentos\GitHub\ELC1098---Mineracao-de-Dados\exerc1.json'
 
 with open(arquivo_json, 'r', encoding='utf-8') as f:
     dados = json.load(f)
